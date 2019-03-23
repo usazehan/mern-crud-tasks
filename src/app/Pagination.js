3899db1b5d5eb2b91fab5a5c9f8e23d7c86b324d
Zehan Sunesara <usazehan@gmail.com>
2019-03-23 18:44:54 -0500

    xx

--- src/app/Pagination.js
+++ src/app/Pagination.js
@@ -1,191 +1,123 @@
-import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
-
-const LEFT_PAGE = "LEFT";
-const RIGHT_PAGE = "RIGHT";
-
-const range = (from, to, step = 1) => {
-    let i = from;
-    const range = [];
-  
-    while (i <= to) {
-      range.push(i);
-      i += step;
-    }
-  
-    return range;
-  };
-  
-  class Pagination extends Component {
+import React, { PropTypes } from 'react';
+ 
+const propTypes = {
+    items: PropTypes.array.isRequired,
+    onChangePage: PropTypes.func.isRequired,
+    initialPage: PropTypes.number   
+}
+ 
+const defaultProps = {
+    initialPage: 1
+}
+ 
+class Pagination extends React.Component {
     constructor(props) {
-      super(props);
-      const { totalRecords = null, pageLimit = 30, pageNeighbours = 0 } = props;
-  
-      this.pageLimit = typeof pageLimit === "number" ? pageLimit : 30;
-      this.totalRecords = typeof totalRecords === "number" ? totalRecords : 0;
-  
-      this.pageNeighbours =
-        typeof pageNeighbours === "number"
-          ? Math.max(0, Math.min(pageNeighbours, 2))
-          : 0;
-  
-      this.totalPages = Math.ceil(this.totalRecords / this.pageLimit);
-  
-      this.state = { currentPage: 1 };
+        super(props);
+        this.state = { pager: {} };
     }
-  
-    componentDidMount() {
-      this.gotoPage(1);
+ 
+    componentWillMount() {
+        this.setPage(this.props.initialPage);
     }
-  
-    gotoPage = page => {
-      const { onPageChanged = f => f } = this.props;
-  
-      const currentPage = Math.max(0, Math.min(page, this.totalPages));
-  
-      const paginationData = {
-        currentPage,
-        totalPages: this.totalPages,
-        pageLimit: this.pageLimit,
-        totalRecords: this.totalRecords
-      };
-  
-      this.setState({ currentPage }, () => onPageChanged(paginationData));
-    };
-  
-    handleClick = (page, evt) => {
-      evt.preventDefault();
-      this.gotoPage(page);
-    };
-  
-    handleMoveLeft = evt => {
-      evt.preventDefault();
-      this.gotoPage(this.state.currentPage - this.pageNeighbours * 2 - 1);
-    };
-  
-    handleMoveRight = evt => {
-      evt.preventDefault();
-      this.gotoPage(this.state.currentPage + this.pageNeighbours * 2 + 1);
-    };
-  
-    fetchPageNumbers = () => {
-      const totalPages = this.totalPages;
-      const currentPage = this.state.currentPage;
-      const pageNeighbours = this.pageNeighbours;
-  
-      const totalNumbers = this.pageNeighbours * 2 + 3;
-      const totalBlocks = totalNumbers + 2;
-  
-      if (totalPages > totalBlocks) {
-        let pages = [];
-  
-        const leftBound = currentPage - pageNeighbours;
-        const rightBound = currentPage + pageNeighbours;
-        const beforeLastPage = totalPages - 1;
-  
-        const startPage = leftBound > 2 ? leftBound : 2;
-        const endPage = rightBound < beforeLastPage ? rightBound : beforeLastPage;
-  
-        pages = range(startPage, endPage);
-  
-        const pagesCount = pages.length;
-        const singleSpillOffset = totalNumbers - pagesCount - 1;
-  
-        const leftSpill = startPage > 2;
-        const rightSpill = endPage < beforeLastPage;
-  
-        const leftSpillPage = LEFT_PAGE;
-        const rightSpillPage = RIGHT_PAGE;
-  
-        if (leftSpill && !rightSpill) {
-          const extraPages = range(startPage - singleSpillOffset, startPage - 1);
-          pages = [leftSpillPage, ...extraPages, ...pages];
-        } else if (!leftSpill && rightSpill) {
-          const extraPages = range(endPage + 1, endPage + singleSpillOffset);
-          pages = [...pages, ...extraPages, rightSpillPage];
-        } else if (leftSpill && rightSpill) {
-          pages = [leftSpillPage, ...pages, rightSpillPage];
+ 
+    setPage(page) {
+        var items = this.props.items;
+        var pager = this.state.pager;
+ 
+        if (page < 1 || page > pager.totalPages) {
+            return;
         }
-  
-        return [1, ...pages, totalPages];
-      }
-  
-      return range(1, totalPages);
-    };
-  
-    render() {
-      if (!this.totalRecords) return null;
-  
-      if (this.totalPages === 1) return null;
-  
-      const { currentPage } = this.state;
-      const pages = this.fetchPageNumbers();
-  
-      return (
-        <Fragment>
-          <nav aria-label="Countries Pagination">
-            <ul className="pagination">
-              {pages.map((page, index) => {
-                if (page === LEFT_PAGE)
-                  return (
-                    <li key={index} className="page-item">
-                      <a
-                        className="page-link"
-                        href="#"
-                        aria-label="Previous"
-                        onClick={this.handleMoveLeft}
-                      >
-                        <span aria-hidden="true">&laquo;</span>
-                        <span className="sr-only">Previous</span>
-                      </a>
-                    </li>
-                  );
-  
-                if (page === RIGHT_PAGE)
-                  return (
-                    <li key={index} className="page-item">
-                      <a
-                        className="page-link"
-                        href="#"
-                        aria-label="Next"
-                        onClick={this.handleMoveRight}
-                      >
-                        <span aria-hidden="true">&raquo;</span>
-                        <span className="sr-only">Next</span>
-                      </a>
-                    </li>
-                  );
-  
-                return (
-                  <li
-                    key={index}
-                    className={`page-item${
-                      currentPage === page ? " active" : ""
-                    }`}
-                  >
-                    <a
-                      className="page-link"
-                      href="#"
-                      onClick={e => this.handleClick(page, e)}
-                    >
-                      {page}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-        </Fragment>
-      );
+ 
+        // get new pager object for specified page
+        pager = this.getPager(items.length, page);
+ 
+        // get new page of items from items array
+        var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
+ 
+        // update state
+        this.setState({ pager: pager });
+ 
+        // call change page function in parent component
+        this.props.onChangePage(pageOfItems);
     }
-  }
-  
-  Pagination.propTypes = {
-    totalRecords: PropTypes.number.isRequired,
-    pageLimit: PropTypes.number,
-    pageNeighbours: PropTypes.number,
-    onPageChanged: PropTypes.func
-  };
-  
-  export default Pagination;
-  
+ 
+    getPager(totalItems, currentPage, pageSize) {
+        // default to first page
+        currentPage = currentPage || 1;
+ 
+        // default page size is 10
+        pageSize = pageSize || 10;
+ 
+        // calculate total pages
+        var totalPages = Math.ceil(totalItems / pageSize);
+ 
+        var startPage, endPage;
+        if (totalPages <= 10) {
+            // less than 10 total pages so show all
+            startPage = 1;
+            endPage = totalPages;
+        } else {
+            // more than 10 total pages so calculate start and end pages
+            if (currentPage <= 6) {
+                startPage = 1;
+                endPage = 10;
+            } else if (currentPage + 4 >= totalPages) {
+                startPage = totalPages - 9;
+                endPage = totalPages;
+            } else {
+                startPage = currentPage - 5;
+                endPage = currentPage + 4;
+            }
+        }
+ 
+        // calculate start and end item indexes
+        var startIndex = (currentPage - 1) * pageSize;
+        var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
+ 
+        // create an array of pages to ng-repeat in the pager control
+        var pages = _.range(startPage, endPage + 1);
+ 
+        // return object with all pager properties required by the view
+        return {
+            totalItems: totalItems,
+            currentPage: currentPage,
+            pageSize: pageSize,
+            totalPages: totalPages,
+            startPage: startPage,
+            endPage: endPage,
+            startIndex: startIndex,
+            endIndex: endIndex,
+            pages: pages
+        };
+    }
+ 
+    render() {
+        var pager = this.state.pager;
+ 
+        return (
+            <ul className="pagination">
+                <li className={pager.currentPage === 1 ? 'disabled' : ''}>
+                    <a onClick={() => this.setPage(1)}>First</a>
+                </li>
+                <li className={pager.currentPage === 1 ? 'disabled' : ''}>
+                    <a onClick={() => this.setPage(pager.currentPage - 1)}>Previous</a>
+                </li>
+                {pager.pages.map((page, index) =>
+                    <li key={index} className={pager.currentPage === page ? 'active' : ''}>
+                        <a onClick={() => this.setPage(page)}>{page}</a>
+                    </li>
+                )}
+                <li className={pager.currentPage === pager.totalPages ? 'disabled' : ''}>
+                    <a onClick={() => this.setPage(pager.currentPage + 1)}>Next</a>
+                </li>
+                <li className={pager.currentPage === pager.totalPages ? 'disabled' : ''}>
+                    <a onClick={() => this.setPage(pager.totalPages)}>Last</a>
+                </li>
+            </ul>
+        );
+    }
+}
+ 
+Pagination.propTypes = propTypes;
+Pagination.defaultProps
+export default Pagination;
